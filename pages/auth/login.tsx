f0946d4520829b5f606ef3ca045fca68f5459698
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-10 15:42:15 -0500

    change login button

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -6,12 +6,14 @@ import { MdFacebook } from "react-icons/md";
 export default function Login() {
     return (
         <Container
+            textAlign={"center"}
             maxW="lg"
             py={{ base: "12", md: "24" }}
             px={{ base: "0", sm: "8" }}
         >
             <Icon
-                boxSize="100px"
+                cursor={"pointer"}
+                boxSize="150px"
                 as={MdFacebook}
                 onClick={() =>
                     signIn("facebook", {
