0225ba36bae901be31caf6867e0b80bd164bcbfc
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-11 11:52:12 -0500

    add framer

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,8 +1,7 @@
-import { Button, Container, Icon } from "@chakra-ui/react";
+import { Container, Icon } from "@chakra-ui/react";
 import { signIn } from "next-auth/react";
 import * as React from "react";
 import { MdFacebook } from "react-icons/md";
-import Options from "https://framer.com/m/Options-wwV8.js@XbOZCNVMFcuBbJbLah8I";
 
 export default function Login() {
     return (
@@ -22,7 +21,6 @@ export default function Login() {
                     })
                 }
             />
-            <Options />
         </Container>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Center, Container, Heading } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
-import { Image } from "@chakra-ui/react";
+import OfferDrink from "https://framer.com/m/offerDrink-N8s7.js@9CRDIPHGMR7EsliQfrb9";
+import Countdown from "react-countdown";
 
 export default function Main() {
     const { data: session, status } = useSession();
@@ -8,8 +9,15 @@ export default function Main() {
     return (
         <Container textAlign={"center"}>
             <Heading py="20px">REVE.S</Heading>
-            {session?.user?.name}
-            <Image src={session?.user?.image || ""} />
+            <Countdown date={Date.now() + 10000} />
+            <Center>
+                {" "}
+                <OfferDrink
+                    // Using default values:
+                    image={session?.user?.image}
+                    name={session?.user?.name}
+                />
+            </Center>
         </Container>
     );
 }
